59be2fca73abec27fb883acc34cba6836e21d55f
exion <1592974207@qq.com>
2023-09-06 14:32:15 +0800

    Swagger2的基本配置完成，异常的统一处理，返回结果统一封装

--- common/service_base/src/main/java/com/swxy_online/service/base/config/Swagger2Config.java
+++ common/service_base/src/main/java/com/swxy_online/service/base/config/Swagger2Config.java
@@ -1,5 +1,6 @@
 package com.swxy_online.service.base.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -8,6 +9,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.function.Predicate;
 
 @Configuration
 @EnableSwagger2
@@ -18,14 +21,29 @@ public class Swagger2Config {
                 .groupName("webApi")
                 .apiInfo(webApiInfo())
                 .select()
-                .paths(PathSelectors.any())
+                .paths(Predicates.and(PathSelectors.regex("/api/.*")))
                 .build();
     }
-
     private ApiInfo webApiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多请关注 https://github.com/swxy-online")
+                .description("涉外学院OnLine在线Api接口定义")
+                .version("1.0")
+                .build();
+    }
+    @Bean
+    public Docket adminApiConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("adminApi")
+                .apiInfo(adminApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
+                .build();
+    }
+    private ApiInfo adminApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("涉外学院OnLine管理员在线Api接口定义")
                 .version("1.0")
                 .build();
     }

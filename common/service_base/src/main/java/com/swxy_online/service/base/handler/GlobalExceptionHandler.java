08226d3b3fc8ad530b747ef67110988aa5cdfcb7
exion <1592974207@qq.com>
2023-09-13 00:07:49 +0800

    测试

--- common/service_base/src/main/java/com/swxy_online/service/base/handler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/swxy_online/service/base/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.swxy_online.service.base.handler;
 
 import com.swxy_online.common.base.result.R;
 import com.swxy_online.common.base.result.ResultCodeEnum;
+import com.swxy_online.common.base.util.ExceptionUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.jdbc.BadSqlGrammarException;
@@ -15,20 +16,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
      public R error(Exception e){
-        log.error(e.getMessage());
+        log.error(ExceptionUtils.getMessage(e));
         return R.error();
      }
      @ExceptionHandler(BadSqlGrammarException.class)
      @ResponseBody
      public R error(BadSqlGrammarException  e){
-
-         log.error(e.getMessage());
+         log.error(ExceptionUtils.getMessage(e));
          return R.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
      }
      @ExceptionHandler(HttpMessageNotReadableException.class)
      @ResponseBody
      public R error(HttpMessageNotReadableException e){
-         log.error(e.getMessage());
+         log.error(ExceptionUtils.getMessage(e));
         return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
      }
 }
